d8639c8938213906b248f86abbf80afa6e88d8fe
Userffft <134155306+Userffft@users.noreply.github.com>
2026-04-27 13:39:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -813,4 +813,4 @@ def handle_get_users():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe)
